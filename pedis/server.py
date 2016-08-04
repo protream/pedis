551688946ed6b435963951410eab6cd4691397ec
protream <protream@gmail.com>
2016-08-04 16:10:34 +0800

    can play ping/pong now:)

--- pedis/server.py
+++ pedis/server.py
@@ -4,11 +4,33 @@
 pedis.server
 ~~~~~~~~~~~~
 
-This is an event-driven tcp server.
+How pedis process a command?
+
+::
+
+    +------>[FileEvent]          +------------->[FileEvent]
+    |            |               |                   |
+    main -> acceptHandler -> createClient -> readQueryFromClient ----+
+                                                                     |
+                                                                     |
+    addReply <- specificCommand <- lookupCommand <- processCommand <-+
+    |               |
+    +----------addReplySds
+    |
+    |
+    +-> sendRepalyToClient
+    |           |
+    +----->[FileEvent]
+
 """
 
+import list
 import event
 import socket
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def serverCron():
@@ -16,21 +38,52 @@ def serverCron():
     loops = server.cronloops
     server.cronloops += 1
 
-    if loops % 3 == 0:
+    if loops % 5 == 0:
         print('{} clients connected.'.format(server.numconnection))
     return 1000
 
 
+class SharedObjects(object):
+    crlf = '\r\n'
+    ok = '+OK\r\n'
+    err = '-ERR\r\n'
+    nil = 'nil\r\n'
+    pong = '+PONG\r\n'
+
+
+class PedisClient(object):
+
+    def __init__(self):
+        self.cobj = None
+        self.querybuf = None
+        self.argc = 0
+        self.argv = None
+        self.flag = 0
+        self.reply = None
+
+
 class PedisServer(object):
+
+    el = event.EventLoop()
+
+    #: All connected clients ared placed in this list
+    clients = list.List()
+
+    numconnection = 0
+
+    #: Times of serverCron executed.
+    cronloops = 0
+
+    #: All supported cmds placed here
+    commands = []
 
     def __init__(self, host='127.0.0.1', port=6374):
         self.host = host
         self.port = port
-        self.fd = self.__tcpServer()
-        self.clients = []
-        self.el = event.EventLoop()
-        self.cronloops = 0
-        self.numconnection = 0
+
+        #: socket object
+        self.sobj = self.__tcpServer()
+
         self.el.createTimeEvent(1000, serverCron, None)
 
     def __initConfig(self):
@@ -44,19 +97,74 @@ class PedisServer(object):
         return s
 
     @classmethod
-    def accept(self, fd):
+    def accept(self, fd, clientData):
         cobj, (host, port) = fd.accept()
         server.numconnection += 1
-        print('Acceptd: {}:{}'.format(host, port))
+        logging.info('Accepted: {}:{}'.format(host, port))
+        self.createClient(cobj)
+
+    @classmethod
+    def createClient(self, cobj):
+        c = PedisClient()
+        c.cobj = cobj
+        c.reply = list.List()
+        self.el.createFileEvent(cobj,
+                                event.READABLE,
+                                self.readQueryFromClient, c)
+        self.clients.addNodeTail(c)
+
+    @classmethod
+    def sendRepalyToClient(self, cobj, client):
+        while client.reply.length:
+            node = client.reply.head
+            cobj.sendall(node.val)
+            client.reply.delNode(node)
+        if client.reply.length == 0:
+            self.el.deleteFileEvent(cobj, event.WRITABLE)
+
+    @classmethod
+    def freeClient(self, cobj):
+        self.el.deleteFileEvent(cobj, event.READABLE)
+        self.el.deleteFileEvent(cobj, event.WRITABLE)
+        cobj.close()
+
+    @classmethod
+    def processCommand(self, c):
+        if c.argv[0] == 'ping':
+            pingCommand(c)
+
+    @classmethod
+    def readQueryFromClient(self, cobj, c):
+        data = cobj.recv(1024)
+        if len(data) == 0:
+            self.freeClient(cobj)
+            return
+        query = data.split()
+        c.argc = len(query)
+        c.argv = query
+        self.processCommand(c)
+
+    @classmethod
+    def addReply(self, client, obj):
+        self.el.createFileEvent(client.cobj,
+                                event.WRITABLE,
+                                self.sendRepalyToClient, client)
+        client.reply.addNodeTail(obj)
 
     def run(self):
-        self.el.createFileEvent(self.fd,
+        self.el.createFileEvent(self.sobj,
                                 event.READABLE,
                                 self.accept, None)
         self.el.main()
 
 #: Global PedisServer object.
 server = PedisServer()
+#: Global shared object.
+shared = SharedObjects()
+
+
+def pingCommand(c):
+    server.addReply(c, shared.pong)
 
 if __name__ == '__main__':
     server.run()
