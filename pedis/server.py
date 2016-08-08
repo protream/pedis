ff44932be18bffe6dce1d3985c1e651999d416f5
protream <protream@gmail.com>
2016-08-08 14:41:48 +0800

    Implement many commands

--- pedis/server.py
+++ pedis/server.py
@@ -26,6 +26,7 @@ pedis.server
 
 import os.path
 import socket
+import random
 import logging
 import event
 from collections import namedtuple
@@ -37,6 +38,9 @@ DEFAULT_DBNUM = 16
 CMD_INLINE  = 1
 CMD_BULK = 2
 
+LIST_HEAD = 0
+LIST_TAIL = 1
+
 
 def serverCron():
     """Server side crond job."""
@@ -45,9 +49,9 @@ def serverCron():
     server.cronloops += 1
 
     for i in range(server.dbnum):
-        numkeys = len(server.dicts[i].keys())
-        if loops % 5 == 0 and numkeys > 0:
-            debug('. DB {}: {} keys in dict'.format(i, numkeys))
+        dbsize = len(server.dicts[i])
+        if loops % 5 == 0 and dbsize > 0:
+            debug('. DB {}: {} keys in dict'.format(i, dbsize))
 
     if loops % 5 == 0:
         debug('. {} clients connected.'.format(server.stat_numconnections))
@@ -77,6 +81,8 @@ class SharedObjects(object):
     select7 = 'select 7\r\n'
     select8 = 'select 8\r\n'
     select8 = 'select 9\r\n'
+    wrongtypeerr = ("-ERR Operation against a key"
+                    "holding the wrong kind of val\r\n")
 
 
 #------------
@@ -105,7 +111,7 @@ class PedisClient(object):
 
 class PedisServer(object):
 
-    #: Python dict use as key:value databse
+    #: Python dict use as key:value database
     dicts = [{} for i in range(DEFAULT_DBNUM)]
 
     dbnum = DEFAULT_DBNUM
@@ -328,9 +334,9 @@ cmd = namedtuple('cmd', ['proc', 'arity', 'flags'])
 class Commands(object):
 
     def __init__(self):
-        """What is a INLINE cmd or a BULK cmd?
+        """What is a INLINE cmd or a BULK cmd flag?
 
-        This is a protocal used by Redis to send datas. For example:
+        This is a trasaction protocal used by Redis to send datas. For example:
 
         ::
            Todo
@@ -338,19 +344,36 @@ class Commands(object):
         """
 
         self.table = {
-            'ping': cmd(self.ping, 1, CMD_INLINE),
-            'echo': cmd(self.echo, 2, CMD_INLINE),
             'get': cmd(self.get, 2, CMD_INLINE),
             'set': cmd(self.set_, 3, CMD_BULK),
             'setnx': cmd(self.setnx, 3, CMD_BULK),
+            'ping': cmd(self.ping, 1, CMD_INLINE),
             'del': cmd(self.del_, 2, CMD_INLINE),
+            'exists': cmd(self.exists, 2, CMD_INLINE),
             'incr': cmd(self.incr, 2, CMD_INLINE),
             'decr': cmd(self.decr, 2, CMD_INLINE),
+            'rpush': cmd(self.rpush, 3, CMD_BULK),
+            'lpush': cmd(self.lpush, 3, CMD_BULK),
+            'rpop': cmd(self.rpop, 2, CMD_INLINE),
+            'lpop': cmd(self.lpop, 2, CMD_INLINE),
+            'llen': cmd(self.llen, 2, CMD_INLINE),
+            'lindex': cmd(self.lindex, 3, CMD_INLINE),
+            'lset': cmd(self.lset, 4, CMD_BULK),
+            'lrange': cmd(self.lrange, 4, CMD_INLINE),
+            'ltrim': cmd(self.ltrim, 4, CMD_INLINE),
+            'lrem': cmd(self.lrem, 4, CMD_BULK),
+            'sadd': cmd(self.sadd, 3, CMD_BULK),
+            'srem': cmd(self.sadd, 3, CMD_BULK),
+            'sismember': cmd(self.sismember, 3, CMD_BULK),
+            'echo': cmd(self.echo, 2, CMD_INLINE),
             'incrby': cmd(self.incrby, 3, CMD_INLINE),
             'decrby': cmd(self.decrby, 3, CMD_INLINE),
-            'exists': cmd(self.exists, 2, CMD_INLINE),
+            'randomkey': cmd(self.randomkey, 1, CMD_INLINE),
+            'flushdb': cmd(self.flushdb, 1, CMD_INLINE),
+            'flushall': cmd(self.flushall, 1, CMD_INLINE),
             'save': cmd(self.save, 1, CMD_INLINE),
             'select': cmd(self.select, 2, CMD_INLINE),
+            'shutdown': cmd(self.shutdown, 1, CMD_INLINE),
         }
 
     def __repr__(self):
@@ -359,6 +382,9 @@ class Commands(object):
     def __len__(self):
         return len(self.table)
 
+    def __getitem__(self, cmd):
+        return self.table[cmd]
+
     def lookup(self, cmd):
         """Look up given cmd.
 
@@ -366,7 +392,10 @@ class Commands(object):
             (found, cmd)
         """
 
-        return (1, self.table[cmd]) if cmd in self.table else (0, None)
+        try:
+            return (1, self[cmd])
+        except KeyError:
+            return (0, None)
 
     def ping(self, c):
         """Test connection, return PONG.
@@ -378,7 +407,7 @@ class Commands(object):
         server.addReply(c, shared.pong)
 
     def echo(self, c):
-        """Echo what you send.
+        """Return what you send.
 
         ::
             ECHO val
@@ -390,7 +419,7 @@ class Commands(object):
     def _setGeneric(self, c, nx):
         """For set and setnx."""
 
-        key, val = c.argv[1], c.argv[2]
+        key, val = c.argv[1:]
 
         if nx and (key in c.dict_):
             server.addReply(shared.one)
@@ -434,11 +463,14 @@ class Commands(object):
             server.addReply(c, shared.nil)
         else:
             val = c.dict_[key]
-            server.addReply(c, val)
-            server.addReply(c, shared.crlf)
+            server.addReply(c, val + '\r\n')
 
     def exists(self, c):
         """Test if a key exists.
+
+        Replys:
+            1: exists
+            0: not exists
 
         ::
             EXISTS key
@@ -471,7 +503,7 @@ class Commands(object):
             server.addReply(c, shared.zero)
 
     def _incrDecr(self, c, x):
-        """Incrment or decrement key by x.
+        """Increment or decrement key by x.
 
         Returns:
             0: key is not integer.
@@ -492,8 +524,7 @@ class Commands(object):
         except KeyError:
             rv = 0
 
-        server.addReply(c, str(rv))
-        server.addReply(c, shared.crlf)
+        server.addReply(c, repr(rv) + '\r\n')
 
     def incr(self, c):
         """Increment the integer value of key.
@@ -546,15 +577,15 @@ class Commands(object):
             SELECT index
         """
 
-        has_err = False
+        has_err = 0
 
         try:
             id_ = int(c.argv[1])
         except ValueError:
-            has_err = True
+            has_err = 1
 
-        if id_ < 0 or id_ >= server.dbnum:
-            has_err = True
+        if _id not in range(server.dbnum):
+            has_err = 1
 
         if has_err:
             server.addReply(c, '-ERR invalid DB index\r\n')
@@ -564,8 +595,501 @@ class Commands(object):
             c.dictid = id_
             server.addReply(c, shared.ok)
 
+    def dbsize(self, c):
+        """Return the number of keys in the current db.
+
+        ::
+            DBSIZE
+        """
+
+        size = len(c.dict_)
+        server.addReply(c, repr(size))
+        server.addReply(c, shared.crlf)
+
+    def _renameGeneric(self, c, nx):
+
+        oldname, newname = c.argv[1:]
+
+        if nx and newname in c.dict_:
+            server.addReply(c, shared.one)
+            return
+
+        try:
+            val = c.dict_.pop(oldname)
+            c.dict_[newname] = val
+            rv = shared.ok
+        except KeyError:
+            rv = shared.zero
+
+        server.addReply(c, rv)
+
+    def rename(self, c):
+        """Rename the old key in the new one, destroing the newname key
+           if it already exists.
+
+        ::
+            RENAME oldname newname
+        """
+
+        self._renameGeneric(c, 0)
+
+    def renamenx(self, c):
+        """Rename the old key in the new one, if the newname key does
+           not already exists.
+
+        ::
+            RENAMENX oldname newname
+        """
+
+        self._renameGeneric(c, 1)
+
+    #--------------------------- list operations -------------------------------
+
+    def _pushGeneric(self, c, where):
+        key, item = c.argv[1:]
+
+        if key not in c.dict_:
+            _l = []
+        else:
+            _l = c.dict_[key]
+            if not isinstance(_l, list):
+                server.addReply(c, shared.wrongtypeerr)
+                return
+
+        if where == LIST_HEAD:
+            _l.insert(0, item)
+        else:
+            _l.append(item)
+
+        c.dict_[key] = _l
+        server.addReply(c, shared.ok)
+
+    def rpush(self, c):
+        """Append an element to the tail of the List value at key.
+
+        ::
+            RPUSH key val
+        """
+
+        self._pushGeneric(c, LIST_TAIL)
+
+    def lpush(self, c):
+        """Append an element to the head of the List value at key.
+
+        ::
+            LPUSH key val
+        """
+
+        self._pushGeneric(c, LIST_HEAD)
+
+    def _valExistsAndIsList(self, c, key):
+        """Check if given key in client's dict, and if the val
+           is list.
+
+        Returns:
+             0: key not exists
+             1: key exists but val is not list
+             2: key exists and val is list
+        """
+
+        if key not in c.dict_:
+            return 0
+
+        _l = c.dict_[key]
+
+        if not isinstance(_l, list):
+            return 1
+
+        return 2
+
+    def llen(self, c):
+        """Return the length of the List value at key.
+
+        ::
+            LLEN key
+        """
+
+        key = c.argv[1]
+
+        if key not in c.dict_:
+            rv = shared.nil
+            return
+
+        _l = c.dict_[key]
+        if not isinstance(_l, list):
+            rv = shared.minus2
+        else:
+            rv = repr(len(_l)) + '\r\n'
+
+        server.addReply(c, rv)
+
+    def lrange(self, c):
+        """Return a range of elements from the List at key.
+
+        ::
+           LRANGE key start end
+        """
+
+        key, start, end = c.argv[1:]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.nil)
+            return
+
+        _l = c.dict_[key]
+        if not isinstance(_l, list):
+            server.addReply(c, shared.wrongtypeerr)
+            return
+
+        try:
+            start = int(start)
+            end = int(end)
+        except ValueError:
+            server.addReply(c, shared.nil)
+            return
+
+        _range = _l[start:end]
+        server.addReply(c, repr(_range) + '\r\n')
+
+    def ltrim(self, c):
+        """Trim the list at key to the specified range of elements.
+
+        ::
+            LTRIM key start end
+        """
+
+        key, start, end = c.argv[1:]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.nil)
+            return
+
+        _l = c.dict_[key]
+        if not isinstance(_l, list):
+            server.addReply(c, shared.wrongtypeerr)
+            return
+
+        try:
+            start = int(start)
+            end = int(end)
+        except ValueError:
+            server.addReply(c, shared.nil)
+            return
+
+        del l[start:end]
+        server.addReply(s, shared.ok)
+
+    def lindex(self, c):
+        """Return the element at index position from the List at key.
+
+        ::
+            LINDEX key index
+        """
+
+        key, index, = c.argv[1:]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.nil)
+            return
+
+        _l = c.dict_[key]
+        if not isinstance(_l, list):
+            server.addReply(c, shared.wrongtypeerr)
+            return
+
+        try:
+            index = int(index)
+        except ValueError:
+            server.addReply(c, shared.nil)
+            return
+
+        try:
+            item = _l[index]
+            server.addReply(c, repr(item) + '\r\n')
+        except IndexError:
+            server.addReply(c, shared.nil)
+
+    def lset(self, c):
+        """Set a new value as the element at index position of the
+           List at key.
+
+        ::
+            LSET key index val
+        """
+
+        key, index, val = c.argv[1:]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.nil)
+            return
+
+        _l = c.dict_[key]
+        if not isinstance(_l, list):
+            server.addReply(c, shared.wrongtypeerr)
+            return
+
+        try:
+            index = int(index)
+        except ValueError:
+            server.addReply(c, shared.nil)
+            return
+
+        try:
+            _l[index] = val
+            server.addReply(c, shared.ok)
+        except IndexError:
+            server.addReply(c, "-ERR index out of range\r\n")
+
+    def lrem(self):
+        """Remove the first-N, last-N, or all the elements matching
+           value from the List at key.
+
+        ::
+            LREM key count value
+        """
+
+        key, count, value = c.argv[1:]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.nil)
+            return
+
+        _l = c.dict_[key]
+        if not isinstance(_l, list):
+            server.addReply(c, shared.wrongtypeerr)
+            return
+
+        try:
+            toremove = int(count)
+        except ValueError:
+            server.addReply(c, shared.minus1)
+
+        removed = 0
+        if toremove >= 0:
+            for x in _l:
+                if toremove and _l[x] == value:
+                    _l.remove(x)
+                    toremove -= 1
+                    removed += 1
+        else:
+            for i, x in enumerate(reversed(_l)):
+                if toremove and _l[x] == value:
+                    _i = -i - 1
+                    _l.pop(_i)
+                    toremove += 1
+                    removed += 1
+
+        server.addReply(c, repr(removed) + '\r\n')
+
+    def _popGeneric(self, c, where):
+
+        key = c.argv[1]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.nil)
+        else:
+            _l = c.dict_[key]
+            if not isinstance(_l, list):
+                server.addReply(c, shared.wrongtypeerr)
+                return
+        try:
+            if where == LIST_HEAD:
+                item = _l.pop(0)
+            else:
+                item = _l.pop()
+            server.addReply(c, repr(item) + '\r\n')
+        except IndexError:
+            server.addReply(c, shared.nil)
+
+    def lpop(self, c):
+        """Return and remove (atomically) the first element of the
+           List at key.
+
+        ::
+            LPOP key
+        """
+
+        self._popGeneric(c, LIST_HEAD)
+
+    def rpop(self, c):
+        """Return and remove (atomically) the last element of the
+           List at key.
+
+        ::
+            RPOP key
+        """
+
+        self._popGeneric(c, LIST_TAIL)
+
+    #---------------------------- set operations -------------------------------
+
+    def sadd(self, c):
+        """Add the specified member to the Set value at key.
+
+        ::
+            SADD key member
+        """
+
+        key, val = c.argv[1], c.argv[2]
+
+        if key not in c.dict_:
+            _s = set()
+        else:
+            _s = c.dict_[key]
+            if not isinstance(_s, set):
+                server.addReply(c, shared.minus2)
+                return
+
+        _s.add(val)
+        c.dict_[key] = _s
+        server.addReply(c, shared.one)
+
+    def srem(self, c):
+        """Remove the specified member from the Set value at key.
+
+        ::
+            SREM key member
+        """
+
+        key, val = c.argv[1], c.argv[2]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.zero)
+        else:
+            _s = c.dict_[key]
+            if not isinstance(_s, set):
+                server.addReply(c, shared.minus2)
+                return
+
+        try:
+            _s.remove(val)
+            rv = shared.one
+        except KeyError:
+            rv = shared.zero
+
+        server.addReply(c, rv)
+
+    def scard(self, c):
+        """Return the number of elements (the cardinality) of the
+           Set at key.
+
+        ::
+            SCARD key
+        """
+
+        key = c.argv[1]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.zero)
+        else:
+            _s = c.dict_[key]
+            if not isinstance(_s, set):
+                server.addReply(c, shared.minus2)
+                return
+
+        server.addReply(c, repr(len(_s)) + '\r\n')
+
+    def sismember(self, c):
+        """Test if the specified value is a member of the Set at key.
+
+        ::
+            SISMEMBER key member
+        """
+
+        key, member = c.argv[1], c.argv[2]
+
+        if key not in c.dict_:
+            server.addReply(c, shared.zero)
+        else:
+            _s = c.dict_[key]
+            if not isinstance(_s, set):
+                server.addReply(c, shared.minus2)
+                return
+
+        if member in _s:
+            rv = shared.one
+        else:
+            rv = shared.zero
+
+        server.addReply(c, rv)
+
+    def sinter(self, c):
+        """Return the intersection between the Sets stored at key1,
+           key2, ..., keyN.
+
+        ::
+            SINTER key1 key2 ... keyN
+        """
+        pass
+
+    def sinterstore(self, c):
+        """Compute the intersection between the Sets stored at key1,
+           key2, ..., keyN, and store the resulting Set at dstkey.
+
+        ::
+            SINTERSTORE dstKey key1 key2 ... keyN
+        """
+        pass
+
+    def smembers(self, c):
+        """Return all the members of the Set value at key.
+
+        ::
+            SMEMBERS key
+        """
+        pass
+
+    def randomkey(self, c):
+        """Return a random key from the key space."""
+
+        keys = list(c.dict_.keys())
+
+        try:
+            rk = random.choice(keys)
+            server.addReply(c, repr(rk) + '\r\n')
+        except IndexError:
+            server.addReply(c, shared.nil)
+
+    def move(self, c):
+        """Move the key from the currently selected DB to the DB
+           having as index dbindex.
+
+        ::
+            MOVE key dbindex
+        """
+
+    def flushdb(self, c):
+        """Remove all the keys of the currently selected DB.
+
+        ::
+            FLUSHDB
+        """
+
+        c.dict_.clear()
+        server.addReply(c, shared.ok)
+
+    def flushall(self, c):
+        """Remove all the keys from all the databases.
+
+        ::
+            FLUSHALL
+        """
+
+        for i in range(server.dbnum):
+            server.dicts[i].clear()
+        server.addReply(c, shared.ok)
+
     def save(self):
         pass
+
+    def shutdown(self, c):
+        wain('# User requested shutdown, saving DB...')
+        if 1:
+            wain('# Server exit now, bye bye...')
+            exit(1)
+        else:
+            wain('# Error trying to save the DB, can\'t exit')
+            server.addReply(c, '-ERR can\'t quit, problems saving the DB\r\n')
 
 
 #---------
